bed3b2c40c1d200272747c9279ad8fbcf03583b6
Glenn Hickey <glenn.hickey@gmail.com>
2021-02-10 10:18:41 -0500

    fix stuff

--- clip-graphs.sh
+++ clip-graphs.sh
@@ -2,8 +2,11 @@
 
 NPROC=4
 
-ls *.clip | parallel -j $NPROC "vg convert -f {} -Q hg38 -B -w . | pigz -p $NPROC > {}.gfa.gz"
+# download the vg files
+for i in `aws s3 ls ${CHROM_BUCKET} | grep vg | awk '{print $4}'`; do echo ${CHROM_BUCKET}${i} ; done | parallel -j $NPROC "aws s3 cp {} ."
+
+# clip them
+ls *.vg | parallel -j $NPROC "clip-vg {} $MASK_BED -r 'mat>2' -r 'pat>1' -r 'CHM13>CHM13.0' > {}.clip"
 
 # for a CHM13 reference:
-ls *.clip | parallel -j $NPROC "vg convert -f {} -Q CHM13 -B -w . | pigz -p $NPROC > {}.gfa.gz"
-
+#ls *.vg | parallel -j $NPROC "clip-vg {} $MASK_BED -r 'mat>2' -r 'pat>1' > {}.clip"
