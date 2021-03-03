0b508d3dfd04e77e87f5399e5264f07678d99bb1
root <glenn.hickey@gmail.com>
2021-03-03 14:08:50 +0000

    update clip-graphs options

--- clip-graphs.sh
+++ clip-graphs.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 CHROM_BUCKET=$1
-MASK_BED=$2
-NPROC=64
+MASK_LEN=$2
+NPROC=8
 
 # download the vg files
 for i in `aws s3 ls ${CHROM_BUCKET} | grep vg | awk '{print $4}'`; do echo ${CHROM_BUCKET}${i} ; done | parallel -j $NPROC "aws s3 cp {} ."
 
 # clip them
-ls *.vg | parallel -j $NPROC "clip-vg {} -b $MASK_BED -f -r 'mat>2' -r 'pat>1' -r 'CHM13>CHM13.0' > {}.clip"
+ls *.vg | parallel -j $NPROC "clip-vg {} -e hg38 -u $MASK_LEN -f -r 'mat>2' -r 'pat>1' -r 'CHM13>CHM13.0' > {}.clip"
 
 # for a CHM13 reference:
-#ls *.vg | parallel -j $NPROC "clip-vg {} -b $MASK_BED -f -r 'mat>2' -r 'pat>1' -r 'hg38>hg38.0' > {}.clip"
+#ls *.vg | parallel -j $NPROC "clip-vg {} -e CHM13 -u $MASK_LEN -f -r 'mat>2' -r 'pat>1' -r 'hg38>hg38.0' > {}.clip"
