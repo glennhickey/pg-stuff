ccf9bb5deb7ba394d895848c6edb9e3fe6eca1b4
glennhickey <glenn.hickey@gmail.com>
2021-04-22 15:03:10 +0000

    fix clean script

--- clean-vcf.sh
+++ clean-vcf.sh
@@ -1,33 +1,26 @@
 #!/bin/bash
 
-set -x
+set -xe
 
-#example ./clean-vcf.sh cactus.vcf GRCh38 1000 hg38.sdf
+#example ./clean-vcf.sh cactus.vcf.gz GRCh38 1000 hg38.fa
 VCF=$1
 PREFIX=$2
 LEN=$3
 REF=$4
-THREADS=20
+THREADS=8
 
 # make the sdf
+rm -rf ${REF}.sdf
 rtg format $REF -o ${REF}.sdf
 
-# first we split out the big variants
-bcftools annotate -x "INFO/AF,INFO/AC" $VCF -i "STRLEN(REF)>${LEN} || STRLEN(ALT)>${LEN}" | sed -e "s/${PREFIX}.//g" | bgzip --threads ${THREADS} > big.vcf.gz
+# then decompose the small variants
+RES1=${VCF::-7}.decomposed.max${LEN}.not.normalized.vcf.gz
+rm -rf ${RES1} ${RES1}.tbi
+bcftools annotate -x "INFO/AF,INFO/AC" $VCF -e "STRLEN(REF)>${LEN} || STRLEN(ALT)>${LEN}" | sed -e "s/${PREFIX}.//g" | rtg vcfdecompose -i - -t ${REF}.sdf --break-indels --break-mnps -o ${RES1}
 
-# then we decompose the small variants
-bcftools annotate -x "INFO/AF,INFO/AC" $VCF -e "STRLEN(REF)>${LEN} || STRLEN(ALT)>${LEN}" | sed -e "s/${PREFIX}.//g" | rtg vcfdecompose -i - -t ${REF}.sdf --break-indels --break-mnps -o small.dc.vcf.gz
+# then we normalize and sort
+RES2=${VCF::-7}.decomposed.max${LEN}.vcf.gz
+bcftools norm --rm-dup all -f $REF ${RES1} | bcftools sort - | bgzip --threads $THREADS > $RES2
+tabix -fp vcf $RES2
 
-# then we make a merged vcf
-bcftools view small.dc.vcf.gz > temp.vcf
-bcftools view -H big.vcf.gz >> temp.vcf
-
-# then sort it
-bcftools sort temp.vcf  | bgzip --threads ${THREADS} > merged.dc.vcf.gz
-tabix -f -p vcf merged.dc.vcf.gz
-rm temp.vcf
-
-# then normalize it
-bcftools norm merged.dc.vcf.gz --rm-dup all -f ${REF} --threads ${THREADS} -Oz > merged.norm.vcf.gz
-tabix -f -p vcf merged.norm.vcf.gz
 
