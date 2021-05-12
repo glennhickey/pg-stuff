43772461a00a0de399a062da846e9f86e7ae990c
root <root@ip-172-31-26-248.us-west-2.compute.internal>
2021-05-12 17:57:57 +0000

    typos

--- cactus-pangenome.sh
+++ cactus-pangenome.sh
@@ -23,7 +23,7 @@ MASK_LEN=100000
 # general toil options
 TOIL_OPTS="--batchSystem mesos --provisioner aws --defaultPreemptable --betaInertia 0 --targetTime 1 --realTimeLogging"
 # jobs get run on r3 clusters
-TOIL_R3_OPTS="--nodeTypes r3.8xlarge:0.7 --maxNodes 20"
+TOIL_R3_OPTS="--nodeTypes r3.8xlarge:0.7 --maxNodes 25"
 # except join, which needs a little more RAM for the whole-genome indexing
 TOIL_JOIN_OPTS="--nodeTypes r5.16xlarge:2.5 --maxNodes 1 --nodeStorage 2000"
 
@@ -134,17 +134,17 @@ fi
 
 # phase 2: divide fasta and PAF into chromosomes
 if [[ $PHASE == "" || $PHASE == "map" || $PHASE == "split" ]]; then
-	 cactus-graphmap-split $JOBSTORE $SEQFILE $MINIGRAPH ${OUTPUT_BUCKET}/${OUTPUT_NAME}.paf  --refContigs "${REFCONTIGS}" --otherContig chrOther --reference $REFERENCE --outDir ${OUTPUT_BUCKET}/chroms-${OUTPUT_NAME} --logFile ${OUTPUT_NAME}.graphmap-split.log ${TOIL_OPTS} ${TOIL_R3_OPTS}
+	 cactus-graphmap-split $JOBSTORE $SEQFILE $MINIGRAPH ${OUTPUT_BUCKET}/${OUTPUT_NAME}.paf  --refContigs ${REFCONTIGS} --otherContig chrOther --reference $REFERENCE --outDir ${OUTPUT_BUCKET}/chroms-${OUTPUT_NAME} --logFile ${OUTPUT_NAME}.graphmap-split.log ${TOIL_OPTS} ${TOIL_R3_OPTS}
 	 aws s3 cp  ${OUTPUT_NAME}.graphmap-split.log ${OUTPUT_BUCKET}/logs-${OUTPUT_NAME}/
 fi
 
 REFCONTIGS="${REFCONTIGS} chrOther"
 
 # phase 3: align each chromosome with Cactus, producing output in both HAL and vg
-if [[ $PHASE == "" || $PHASE == "map" || $PHASE == "split" || phase == "align" ]]; then
+if [[ $PHASE == "" || $PHASE == "map" || $PHASE == "split" || $PHASE == "align" ]]; then
 	 aws s3 cp ${OUTPUT_BUCKET}/chroms-${OUTPUT_NAME}/chromfile.txt ./chromfile-${OUTPUT_NAME}.txt
-	 aws s3 sync ${OUTPUT_BUCKET}/chroms-${OUTPUT_NAME}/seqfiles ./seqfiles-${OUTPUT_NAME}.txt 
-	 sed -i -e 's/seqfiles/seqfiles-${OUTPUT_NAME}/g' ./chromfile-${OUTPUT_NAME}.txt
+	 aws s3 sync ${OUTPUT_BUCKET}/chroms-${OUTPUT_NAME}/seqfiles ./seqfiles-${OUTPUT_NAME} 
+	 sed -i -e "s/seqfiles/seqfiles-${OUTPUT_NAME}/g" ./chromfile-${OUTPUT_NAME}.txt
 
 	 cactus-align-batch $JOBSTORE ./chromfile-${OUTPUT_NAME}.txt ${OUTPUT_BUCKET}/align-batch-${OUTPUT_NAME} --alignCores 32 --alignOptions "--pafInput --pangenome --outVG --realTimeLogging --pafMaskFilter ${MASK_LEN} --barMaskFilter ${MASK_LEN} --reference ${REFERENCE}" --logFile ${OUTPUT_NAME}.align.log ${TOIL_OPTS} ${TOIL_R3_OPTS}
 	 aws s3 cp  ${OUTPUT_NAME}.align.log ${OUTPUT_BUCKET}/logs-${OUTPUT_NAME}/
@@ -162,6 +162,7 @@ fi
 
 # phase 4: merge the chromosome output into whole genome HAL, GFA, VCF, XG, SNARLS and GBWT
 cactus-graphmap-join $JOBSTORE --outDir $OUTPUT_BUCKET --outName $OUTPUT_NAME --reference $REFERENCE  $JOIN_OPTS --vg $(for i in $REFCONTIGS; do echo ${OUTPUT_BUCKET}/align-batch-${OUTPUT_NAME}/${i}.vg) --hal $(for i in $REFCONTIGS; do echo ${OUTPUT_BUCKET}/align-batch-${OUTPUT_NAME}/${i}.hal) --logFile ${OUTPUT_NAME}.join.log ${TOIL_OPTS} ${TOIL_JOIN_OPTS}
+
 aws s3 cp  ${OUTPUT_NAME}.join.log ${OUTPUT_BUCKET}/logs-${OUTPUT_NAME}/
 
 date
