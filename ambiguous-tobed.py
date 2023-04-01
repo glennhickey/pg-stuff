e6ae72d2c0b5066e9457b110272f618bbbe5ef5f
Glenn Hickey <glenn.hickey@gmail.com>
2023-04-01 15:09:24 -0400

    bug

--- ambiguous-tobed.py
+++ ambiguous-tobed.py
@@ -21,5 +21,9 @@ with open(sys.argv[1], 'r') as split_log:
             coverage_threshold = float(toks[8][:-1])
             uf = float(toks[9][3:])
             uf_threshold = float(toks[11][:-1])
+            pipe_pos = contig_name.find('|')
+            dot_pos = contig_name.find('.')
+            if dot_pos > 0 and pipe_pos > 0 and dot_pos < pipe_pos:
+                contig_name = contig_name.replace('.', '#', 1)
             contig_name = contig_name.replace('id=', '').replace('|', '#')
             print('{}\t{}\t{}\t{}'.format(contig_name, 0, contig_length, '{};{};{};{}'.format(query_coverage, coverage_threshold, uf, uf_threshold)))
