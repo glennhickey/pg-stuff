3063363d965fccb88a1116a238f3f268508d8701
Glenn Hickey <glenn.hickey@gmail.com>
2025-07-23 13:35:25 -0400

    update

--- slurm-deconstruct.sh
+++ slurm-deconstruct.sh
@@ -6,6 +6,7 @@ REF=$2
 L=$3
 OUTPUT_DIR=$4
 OUTPUT_NAME=$5
+OUTPUT_FA=${OUTPUT_NAME::-7}.fa
 
 mkdir -p $OUTPUT_DIR
 rm -rf ${OUTPUT_DIR}/*.vcf*
@@ -15,12 +16,16 @@ for VG in ${VG_DIR}/*.vg; do
 	BASE=$(basename $VG)
 	BASE=${BASE::-3}
 	VCF=${OUTPUT_DIR}/${BASE}.${REF}.${L}.vcf.gz
-	#$sbatch -W ./slurm-deconstruct-one.sh $VG "-n -L ${L} -P ${REF}" $VCF &
-	./slurm-deconstruct-one.sh $VG "-n -L ${L} -P ${REF}" $VCF &
+	FASTA=${OUTPUT_DIR}/${BASE}.${REF}.${L}.fa
+	$sbatch -W ./slurm-deconstruct-one.sh $VG "-n -L ${L} -P ${REF} -f ${FASTA}" $VCF &
+	#./slurm-deconstruct-one.sh $VG "-n -L ${L} -P ${REF} -f ${FASTA}" $VCF &
     fi
 done
 
 wait
+
+cat ${OUTPUT_DIR}/*.fa > ${OUTPUT_FA}
+cat ${OUTPUT_DIR}/*.fa.nesting.tsv > ${OUTPUT_FA}.nesting.tsv
 
 bcftools concat ${OUTPUT_DIR}/*.vcf.gz | bgzip > ${OUTPUT_NAME}
 tabix -fp vcf ${OUTPUT_NAME}.vcf.gz
